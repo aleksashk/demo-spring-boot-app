a715290050d98ed89e83ee443e7efa3c06661b29
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-13 18:30:48 +0300

    refactoring ColumnController

--- src/main/java/by/flameksandr/demospringbootapp/controllers/ColumnController.java
+++ src/main/java/by/flameksandr/demospringbootapp/controllers/ColumnController.java
@@ -2,15 +2,14 @@ package by.flameksandr.demospringbootapp.controllers;
 
 import by.flameksandr.demospringbootapp.model.Column;
 import by.flameksandr.demospringbootapp.repositories.ColumnRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "column")
 public class ColumnController {
 
@@ -21,18 +20,31 @@ public class ColumnController {
     }
 
     @PostMapping(path = "all")
-    @ResponseBody
     public List<Column> getAllColumns() {
         return columnRepository.findAll();
     }
 
     @PostMapping(path = "add")
-    @ResponseBody
     public Column addColumn(@RequestParam String title) {
         Column column = new Column();
         column.setTitle(title);
 
         columnRepository.save(column);
         return column;
+    }
+
+    @PostMapping(path = "delete")
+    public String deleteColumn(@RequestParam String id) {
+        Column column = columnRepository.findById(Integer.parseInt(id));
+        columnRepository.delete(column);
+        return "ok";
+    }
+
+    @PostMapping(path = "rename")
+    public String deleteColumn(@RequestParam String id, String title) {
+        Column column = columnRepository.findById(Integer.parseInt(id));
+        column.setTitle(title);
+        columnRepository.save(column);
+        return "ok";
     }
 }
